379845548dd2ce02912dd429dd387fd81eb58176
Rui Costa <ruicosta.pr@gmail.com>
2021-10-05 11:31:02 +0100

    license

---
+++ analog-clock.js
@@ -0,0 +1,518 @@
+/**
+ *
+ * @param {string} clockid
+ */
+function BuildClock(params) {
+    const self = this;
+    var elementid = null;
+    var _width = 320; // Base Build Size px
+
+    var canvasSize = 320; // Component default width
+    var dataini = new Date(); // Component Start Date - Default Current Time
+
+    // Component Options
+    var showDigital = false;
+    var paramsDigital = null;
+
+    // Handle Params
+    if (typeof (params) == "object") {
+        if (!params.clockid) {
+            console.log("NOT FOUND: clockid");
+            return;
+        }
+        elementid = params.clockid;
+    } else if (typeof (params) == "string") {
+        // Set Module Element ID
+        elementid = params;
+    }
+    else {
+        console.log("PARAMS NOT VALID");
+        return;
+    }
+
+    // Object Container
+    var container = document.getElementById(elementid);
+    // Set Canvas ID
+    var canvasid = elementid + "-canvas";
+
+    // Setting Module Properties
+    setParams(params);
+
+    // Html Cnavas
+    var canvashtml = `<div style="width:100%"><canvas id="${canvasid}" width="${canvasSize}" height="${canvasSize}"></canvas></div>`;
+    container.innerHTML = canvashtml;
+    container.style.backgroundColor = "transparent";
+    container.style.width = "100%";
+
+    // Set Title
+    if (params.title) {
+        showTitle(params.title);
+    }
+
+    // Clock Radius
+    var clockRadius = _width * 0.5;
+
+    // Canvas Html Element
+    this.canvas = document.getElementById(canvasid);
+    this.canvas.style.margin = "auto";
+    this.canvas.style.backgroundColor = "rgb(76 0 0)";
+    this.canvas.style.borderRadius = canvasSize * 0.5 + "px";
+    this.canvas.style.display = "block";
+
+    // Component Colors
+    this.colors = {};
+    this.colors.center = "rgb(225 12 12)";
+    this.colors.secondPointer = "rgb(221 195 195)";
+    this.colors.minutePointer = "#fffddd";
+    this.colors.hourPointer = "#fffddd";
+    this.colors.ticks = "white";
+    this.colors.bigTicks = "white";
+    this.colors.numbers = "white";
+    this.colors.borders = "#F5EEF8";
+    // Clock Font Style
+    this.fontStyle = "24px Arial";
+
+    // Center Point
+    var centerPoint = {
+        x: clockRadius,
+        y: clockRadius,
+    };
+    // Pointers lenght
+    var endPoint = {
+        x: centerPoint.x,
+        y: centerPoint.y - clockRadius * 0.72,
+    };
+    // Numbers Position
+    var numberPoint = {
+        x: centerPoint.x,
+        y: centerPoint.y - clockRadius * 0.77,
+    };
+
+    // Loop at each second
+    setInterval(build, 1000);
+
+    /**
+     * Build Clock
+     * -----------
+     */
+    function build() {
+
+        // Create Temp Context
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        canvas.width = _width;
+        canvas.height = _width;
+
+        // Draw Border
+        drawBorder(ctx);
+
+        // Draw Numbers
+        drawNumbers(ctx);
+
+        let sec = self.leadingZeros(dataini.getSeconds());
+        let min = self.leadingZeros(dataini.getMinutes());
+        let hour = self.leadingZeros(dataini.getHours());
+        // Hour Pointer
+        let incHour = 2 / 12;
+        let incMin = incHour * (min / 60);
+        drawHour(ctx, hour * incHour + incMin);
+
+        // Draw Minutes Pointer
+        drawMinutes(ctx, (min * 2) / 60);
+
+        // Draw Seconds Pointer
+        drawSeconds(ctx, sec * (2 / 60));
+
+        // Draw Center
+        drawCenter(ctx);
+
+        // Draw Ticks
+        drawTicks(ctx);
+        drawBigTicks(ctx);
+
+        // Get Main Context
+        var mctx = self.canvas.getContext("2d");
+        // Clear canvas
+        mctx.clearRect(0, 0, self.canvas.width, self.canvas.height);
+
+        // Draw  Clock
+        mctx.drawImage(
+            canvas,
+            0,
+            0,
+            canvas.width,
+            canvas.height,
+            0,
+            0,
+            self.canvas.width,
+            self.canvas.height
+        );
+
+        // Draw Digital
+        if (showDigital === true) {
+            drawDigital(paramsDigital);
+        }
+
+        // Set current time
+        dataini.setTime(dataini.getTime() + 1000);
+    };
+
+    /**
+     * Draw Hour Pointersolid 5px rgb(169, 149, 5)
+     * -----------
+     * @param {context} ctx
+     * @param {float} rotation
+     */
+    var drawHour = function (ctx, rotation) {
+        ctx.beginPath();
+        ctx.lineCap = "round";
+        ctx.strokeStyle = self.colors.hourPointer;
+        // translate to centerPoint
+        ctx.translate(centerPoint.x, centerPoint.y);
+
+        // rotate some angle (radians)
+        ctx.rotate(rotation * Math.PI); // 0.25 = 45°
+        // translate back
+        ctx.translate(-centerPoint.x, -centerPoint.y);
+
+        // draw line
+        ctx.moveTo(centerPoint.x, centerPoint.y + 20);
+        ctx.lineTo(endPoint.x, endPoint.y * 2.3);
+        ctx.lineWidth = 7;
+        ctx.stroke();
+        ctx.closePath();
+
+        // reset transforms
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    };
+
+    /**
+     * Draw Minute Pointer
+     * -----------
+     * @param {context} ctx
+     * @param {float} rotation
+     */
+    var drawMinutes = function (ctx, rotation) {
+        ctx.beginPath();
+        ctx.lineCap = "round";
+        ctx.strokeStyle = self.colors.minutePointer;
+        // translate to centerPoint
+        ctx.translate(centerPoint.x, centerPoint.y);
+
+        // rotate some angle (radians)
+        ctx.rotate(rotation * Math.PI); // 0.25 = 45°
+        // translate back
+        ctx.translate(-centerPoint.x, -centerPoint.y);
+
+        // draw line
+        ctx.moveTo(centerPoint.x, centerPoint.y + 20);
+        ctx.lineTo(endPoint.x, endPoint.y * 1.5);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+        ctx.closePath();
+
+        // reset transforms
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    };
+
+    /**
+     * Draw Seconds Pointer
+     * -----------
+     * @param {context} ctx
+     * @param {float} rotation
+     */
+    var drawSeconds = function (ctx, rotation) {
+        ctx.beginPath();
+        ctx.lineCap = "round";
+        ctx.strokeStyle = self.colors.secondPointer;
+        // translate to centerPoint
+        ctx.translate(centerPoint.x, centerPoint.y);
+
+        // rotate some angle (radians)
+        ctx.rotate(rotation * Math.PI); // 0.25 = 45°
+        // translate back
+        ctx.translate(-centerPoint.x, -centerPoint.y);
+
+        // draw line
+        ctx.moveTo(centerPoint.x, centerPoint.y + 20);
+        ctx.lineTo(endPoint.x, endPoint.y * 1.2);
+        ctx.lineWidth = 3;
+        ctx.stroke();
+        ctx.closePath();
+
+        // reset transforms
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    };
+
+    /**
+     * Draw Border
+     * -----------
+     * @param {context} ctx
+     */
+    var drawBorder = function (ctx) {
+        ctx.beginPath();
+        ctx.arc(
+            centerPoint.x,
+            centerPoint.y,
+            clockRadius * 0.95,
+            0,
+            2 * Math.PI
+        );
+        ctx.arc(
+            centerPoint.x,
+            centerPoint.y,
+            clockRadius * 0.93,
+            0,
+            2 * Math.PI
+        );
+        ctx.strokeStyle = self.colors.borders;
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        ctx.closePath();
+
+        // reset transforms
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    };
+
+    /**
+     * Draw Numbers
+     * -----------
+     * @param {context} ctx
+     * @param {float} rotation
+     */
+    var drawNumbers = function (ctx) {
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = self.colors.numbers;
+        ctx.fillStyle = self.colors.numbers;
+        ctx.font = self.fontStyle;
+
+        let inc = 2 / 12;
+        let rotation = inc * 0;
+
+        for (let hour = 1; hour < 13; hour++) {
+            rotation = inc * hour;
+            ctx.beginPath();
+
+            // translate to centerpoint
+            ctx.translate(centerPoint.x - 7, centerPoint.y + 9);
+
+            // rotate some angle (radians)
+            ctx.rotate(rotation * Math.PI);
+
+            // translate back
+            ctx.translate(-centerPoint.x, -centerPoint.y);
+
+            ctx.translate(numberPoint.x, numberPoint.y);
+            ctx.rotate(-rotation * Math.PI);
+            ctx.translate(-numberPoint.x, -numberPoint.y);
+
+            ctx.strokeText(hour, numberPoint.x, numberPoint.y);
+            ctx.fillText(hour, numberPoint.x, numberPoint.y);
+
+            // close path
+            ctx.closePath();
+
+            // reset transforms
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+        }
+    };
+
+    /**
+     * Draw Ticks
+     * -----------
+     * @param {context} ctx
+     * @param {float} rotation
+     */
+    var drawTicks = function (ctx) {
+        let inc = 2 / 60;
+        let rotation = inc * 0;
+
+        for (let tick = 1; tick < 61; tick++) {
+            rotation = inc * tick;
+            ctx.beginPath();
+            ctx.strokeStyle = self.colors.ticks;
+            // translate to centerpoint
+            ctx.translate(centerPoint.x, centerPoint.y);
+
+            // rotate some angle (radians)
+            ctx.rotate(rotation * Math.PI);
+
+            // translate back
+            ctx.translate(-centerPoint.x, -centerPoint.y);
+
+            // draw line
+            ctx.moveTo(endPoint.x, endPoint.y - 26);
+            ctx.lineTo(endPoint.x, endPoint.y - 32);
+            ctx.lineWidth = 1;
+            ctx.stroke();
+
+            // close path
+            ctx.closePath();
+
+            // reset transforms
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+        }
+    };
+
+    /**
+     * Draw Big Ticks
+     * -----------
+     * @param {context} ctx
+     * @param {float} rotation
+     */
+    var drawBigTicks = function (ctx) {
+        let inc = 2 / 12;
+        let rotation = inc * 0;
+
+        for (let tick = 1; tick < 61; tick++) {
+            rotation = inc * tick;
+            ctx.beginPath();
+            ctx.strokeStyle = self.colors.bigTicks;
+            // translate to centerpoint
+            ctx.translate(centerPoint.x, centerPoint.y);
+
+            // rotate some angle (radians)
+            ctx.rotate(rotation * Math.PI);
+
+            // translate back
+            ctx.translate(-centerPoint.x, -centerPoint.y);
+
+            // draw line
+            ctx.moveTo(endPoint.x, endPoint.y - 25);
+            ctx.lineTo(endPoint.x, endPoint.y - 32);
+            ctx.lineWidth = 3;
+            ctx.stroke();
+
+            // close path
+            ctx.closePath();
+
+            // reset transforms
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+        }
+    };
+
+    /**
+     * Draw Center
+     * -----------
+     * @param {context} ctx
+     * @param {float} rotation
+     */
+    var drawCenter = function (ctx) {
+        ctx.beginPath();
+        ctx.arc(centerPoint.x, centerPoint.y, 2, 0, 2 * Math.PI);
+        ctx.strokeStyle = self.colors.center;
+        ctx.lineWidth = 5;
+        ctx.stroke();
+        ctx.closePath();
+        // reset transforms
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    };
+
+    /**
+     * Draw Digital
+     * -----------
+     * @param {params} object
+     */
+    var drawDigital = function (params = null) {
+        let sec = self.leadingZeros(dataini.getSeconds());
+        let min = self.leadingZeros(dataini.getMinutes());
+        let hour = self.leadingZeros(dataini.getHours());
+        let str = `${hour}:${min}:${sec}`;
+
+        let digitalid = elementid + "-digital";
+        let olddiv = document.getElementById(digitalid);
+        if (olddiv !== null) {
+            olddiv.parentNode.removeChild(olddiv);
+        }
+
+        let div = document.createElement("div");
+        div.setAttribute("id", digitalid);
+        container.append(div);
+
+        let digital = document.getElementById(digitalid);
+        digital.innerHTML = str;
+        digital.style.width = "100%";
+        digital.style.textAlign = "center";
+        digital.style.marginTop = "5px";
+        digital.style.font = "16px Arial";
+    };
+
+    /**
+     * Show Title
+     * -----------
+     * @param {string} title
+     */
+    function showTitle(txtTitle) {
+        let titleid = elementid + "-title";
+        let olddiv = document.getElementById(titleid);
+        if (olddiv !== null) {
+            olddiv.parentNode.removeChild(olddiv);
+        }
+
+        let div = document.createElement("div");
+        div.setAttribute("id", titleid);
+        container.prepend(div);
+
+        let title = document.getElementById(titleid);
+        title.innerHTML = txtTitle;
+        title.style.width = "100%";
+        title.style.textAlign = "center";
+        title.style.marginBottom = "5px";
+        title.style.fontSize = "16px";
+    };
+
+    /**
+     * Leading Zeros
+     * -------------
+     * @param {int} val Value to return
+     * @returns string representing a number with 2 or more digits
+     */
+    this.leadingZeros = function (val) {
+        if (val < 10) {
+            val = "0" + val;
+        }
+        return val;
+    };
+
+    /**
+     * Setting Properties
+     * ------------------
+     * @param {object} params Setting available properties
+     */
+    function setParams(params) {
+        if (params.size) {
+            canvasSize = params.size;
+        }
+        if (params.date) {
+            dataini = new Date(params.date);
+        }
+        if (params.showDigital) {
+            showDigital = params.showDigital;
+        }
+    };
+};
+
+BuildClock.prototype.borderStyle = function (style) {
+    this.canvas.style.border = style;
+};
+
+BuildClock.prototype.backgroundColor = function (color) {
+    this.canvas.style.backgroundColor = color;
+};
+
+BuildClock.prototype.borderRadius = function (radius) {
+    this.canvas.style.borderRadius = radius;
+};
+
+BuildClock.prototype.centerColor = function (color) {
+    this.colors.center = color;
+};
+
+BuildClock.prototype.showDigital = function (params = null, show = true) {
+    showDigital = show;
+    paramsDigital = params;
+};
+
+BuildClock.prototype.setTime = function (timestring) {
+    dataini = new Date(timestring);
+};
--- page.js
+++ page.js
@@ -19,12 +19,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
       date: dt.toISOString(),
     }
   );
+  var dt = new Date();
+  dt.setHours(dt.getHours() - 1);
+  const clock3 = new BuildClock(
+    {
+      clockid: "clock3",
+      size: 160,
+      title: "Açores",
+      showDigital: true,
+      date: dt.toISOString(),
+    }
+  );
   //const clock = new BuildClock("clock");
   //clock.showDigital();
   //clock.setTime("2020-12-17T21:59:45");
-  /*clock.borderStyle("1px solid rgb(1, 126, 59)");
+
+  clock.borderStyle("1px solid rgb(1, 126, 59)");
   clock.backgroundColor("rgb(99 167 114)");
-  clock.borderRadius("100px");
+  //clock.borderRadius("100px");
   clock.centerColor("red");
   clock.colors.secondPointer = "rgb(34, 116, 71)";
   clock.colors.hourPointer = "rgb(34, 116, 71)";
@@ -32,5 +44,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
   clock.colors.numbers = "rgb(188, 232, 208)";
   clock.colors.borders = "rgb(1, 126, 59)";
   clock.colors.ticks = "rgb(188, 232, 208)";
-  clock.colors.bigTicks = "rgb(188, 232, 208)";*/
+  clock.colors.bigTicks = "rgb(188, 232, 208)";
+
+  clock3.borderStyle("1px solid rgb(32, 135, 204)");
+  clock3.backgroundColor("rgb(1, 76, 126)");
+  clock3.centerColor("red");
+  clock3.colors.secondPointer = "rgb(32, 135, 204)";
+  clock3.colors.hourPointer = "rgb(32, 135, 204)";
+  clock3.colors.minutePointer = "rgb(32, 135, 204)";
+  clock3.colors.numbers = "rgb(188, 232, 208)";
+  clock3.colors.borders = "rgb(32, 135, 204)";
+  clock3.colors.ticks = "rgb(188, 232, 208)";
+  clock3.colors.bigTicks = "rgb(188, 232, 208)";
 });
